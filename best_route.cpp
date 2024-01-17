d92b90cb2331f784f6c46bedace32c04ed74a6a2
lidka-lappo <lidia.lappo@gmail.com>
2024-01-17 22:04:00 +0100

    Working finding routes. Test on small example correct

--- best_route.cpp
+++ best_route.cpp
@@ -24,6 +24,10 @@ string names[20] = {"empty", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "
 int lightisotop[100] = {0, 1, 3, 4, 6, 7, 8, 10, 11, 14, 15, 17, 19, 21, 22, 25, 26, 29, 30, 33, 34, 38, 38, 42, 42, 44, 45, 50, 48, 53, 54, 59, 59, 63, 64, 69, 69, 73, 73, 76, 77, 81, 81, 85, 85, 89, 90, 92, 94, 96, 99, 103, 104, 108, 108, 112, 114, 117, 121, 121, 125, 128, 129, 130, 134, 135, 139, 140, 143, 144, 149, 150, 151, 155, 157, 159, 161, 164, 166, 170, 171, 176, 178, 184, 186, 191, 193, 197, 201, 205, 208, 211, 215, 219, 228, 223, 233, 233, 237, 240};
 int heavyisotop[100] = {0, 7, 10, 12, 16, 21, 22, 24, 26, 31, 34, 37, 40, 43, 44, 46, 48, 51, 53, 56, 58, 61, 64, 67, 70, 73, 75, 77, 82, 82, 85, 87, 90, 92, 95, 101, 102, 106, 108, 111, 114, 117, 119, 122, 125, 128, 131, 132, 134, 137, 140, 142, 145, 147, 150, 152, 154, 157, 158, 161, 163, 165, 168, 169, 172, 174, 176, 178, 180, 181, 185, 188, 190, 194, 197, 199, 203, 205, 208, 210, 216, 217, 220, 224, 227, 229, 229, 233, 234, 236, 238, 239, 243, 252, 252, 252, 252, 253, 256, 257};
 
+struct Index {
+    int i;
+    int j;
+};
 
 struct ProductionData {
     std::string isotope;
@@ -33,10 +37,9 @@ struct ProductionData {
 
 struct route {
     std::vector<std::string> isotopes;
-    std::vector<int> parent;
+    std::string parent_isotope;
     std::vector<long double> frequency;
     long double final_frequency;
-
     bool finish= false;
 };
 
@@ -118,21 +121,6 @@ void open_data(vector<ProductionData> *productionData)
 
 }
 
-/*ProductionData get_vektor(int Z, int A, vector<ProductionData> *productionData )
-{
-    ProductionData tmp;
-      for (const auto& data : productionData) {
-        string tmp_name = names[Z] + to_string(A);
-        if(data.isotope.compare(tmp_name))
-        {
-            cout<<data.isotope<<endl;
-            return data;
-        }
-      }
-      return tmp;
-
-
-}*/
 
 ProductionData get_vektor(int Z, int A, std::vector<ProductionData>& productionData) {
     ProductionData tmp;
@@ -161,24 +149,49 @@ ProductionData get_vektor(string name, std::vector<ProductionData>& productionDa
 }
 
 
+Index find_the_parent(string parent_name, vector<route> &MainQueue_second)
+{
+    Index index;
+    index.i = 0;
+    index.j = 0;
+    //cout<<"szukam"<<parent_name<<endl;
+    for(int i = 0; i< MainQueue_second.size(); i++)
+    {
+        for(int j = 0; j< MainQueue_second[i].isotopes.size(); j++)
+        {
+            if(MainQueue_second[i].isotopes[j] == parent_name)
+            {
+                index.i =i;
+                index.j =j;
+            }
+        }
+    }
+    return index;
+}
+
+
 
 int main() {
 
     //READING DATA FROM FILE
     vector<route> routes;
     route MainQueue;
-    route MainQueue_second;
+    vector<route> MainQueue_second;
     std::vector<ProductionData> productionData;
     open_data(&productionData);
     int Z = 1;
     int A = 1;
+
     route tmp_route;
     tmp_route.isotopes.push_back(names[Z] + to_string(A));
     tmp_route.frequency.push_back(1);
+    tmp_route.finish = true;
+
     MainQueue.isotopes.push_back(names[Z] + to_string(A));
     MainQueue.frequency.push_back(1);
-    MainQueue_second.isotopes.push_back(names[Z] + to_string(A));
-    MainQueue_second.frequency.push_back(1);
+    
+    MainQueue_second.push_back(tmp_route);
+
     int i = 0;
     string tmp_current_name;            
     long double  tmp_current_freq; 
@@ -194,6 +207,7 @@ int main() {
         //cout<<current.isotope<<endl;
         if(current.isotope!="null")
         {
+            route full_route;
                         
             for(int j =0; j<current.isotopes.size()-1; j++)
             {
@@ -201,28 +215,20 @@ int main() {
                 
                 MainQueue.isotopes.push_back(current.isotopes[j]);
                 MainQueue.frequency.push_back(current.quantities[j]/(1000000.0));
-                MainQueue_second.isotopes.push_back(current.isotopes[j]);
-                MainQueue_second.frequency.push_back(current.quantities[j]/(1000000.0));
-                MainQueue_second.parent.push_back(i);
+                full_route.isotopes.push_back(current.isotopes[j]);
+                full_route.frequency.push_back(current.quantities[j]/(1000000.0));
+                full_route.parent_isotope = current.isotope;
                 //std::cout << "Isotope: " << current.isotopes[j] << " Quantity: " << current.quantities[j] << endl;
             }
+            MainQueue_second.push_back(full_route);
             route tmp_route;
             tmp_route.isotopes.push_back(current.isotopes[current.isotopes.size()-1]);
             tmp_route.frequency.push_back(current.quantities[current.isotopes.size()-1]/(1000000.0));
             tmp_route.finish = false;
-            tmp_route.parent.push_back(i);
+            tmp_route.parent_isotope = current.isotope;
             routes.push_back(tmp_route);               
         }
-       /* else
-        {
-            route tmp_route;
-            tmp_route.isotopes.push_back(tmp_current_name);
-            tmp_route.frequency.push_back(tmp_current_freq);
-            tmp_route.finish = false;
-            tmp_route.parent.push_back(i);
-            routes.push_back(tmp_route); 
-        }*/
-        //cout<<"next current "<<MainQueue.isotopes[MainQueue.isotopes.size()-1]<<endl;
+       
         tmp_current_name = MainQueue.isotopes[MainQueue.isotopes.size()-1];
         tmp_current_freq = MainQueue.frequency[MainQueue.isotopes.size()-1];
         current = get_vektor(tmp_current_name, productionData);
@@ -237,20 +243,30 @@ int main() {
  /////////////////////////////////////////////////////////////////   
 // GETTING ROUTES
 //////////////////////////////////////////////////////////////////
-    
+
+ 
+
+
     for(int i = 0;  i <routes.size(); i++)
-    {
-        int tmp = routes[i].parent[0];
-        if(!routes[i].finish)
-        {                 
-            routes[i].isotopes.push_back(MainQueue_second.isotopes[tmp]);
-            routes[i].frequency.push_back(MainQueue_second.frequency[tmp]);
-            routes[i].frequency.push_back(MainQueue_second.parent[tmp]);
-        }
-        if(tmp==0)
+    { 
+        
+        route current;
+        current=routes[i];
+        while(!current.finish)
         {
-            routes[i].finish = true;
+            Index parent_index = find_the_parent(current.parent_isotope, MainQueue_second);
+            //cout<< parent_index.i<<parent_index.j<<endl;
+            routes[i].isotopes.push_back(MainQueue_second[parent_index.i].isotopes[parent_index.j]);
+            routes[i].frequency.push_back(MainQueue_second[parent_index.i].frequency[parent_index.j]);
+            route tmp_current;
+            tmp_current.isotopes.push_back(MainQueue_second[parent_index.i].isotopes[parent_index.j]);
+            tmp_current.finish = MainQueue_second[parent_index.i].finish;
+            tmp_current.parent_isotope = MainQueue_second[parent_index.i].parent_isotope;
+            current = tmp_current;
+
         }
+        routes[i].finish=true;
+
     }
 
 
@@ -258,10 +274,10 @@ int main() {
     for (const auto& route : routes) {
         std::cout << "Route Isotopes and Quantities: " << std::endl;
         for (size_t i = 0; i < route.isotopes.size(); ++i) {
-            std::cout<< route.parent[i]<<":"<<route.isotopes[i] << ", ";
+            std::cout<<route.isotopes[i] << ", ";
            // std::cout << "Isotope: " << route.isotopes[i] << " Frequency: " << route.frequency[i]<< std::endl;
         }
-       // std::cout << "Index "<< route.parent << std::endl;
+        std::cout << "Finish? "<< route.finish << std::endl;
         std::cout << std::endl;
 
     }
